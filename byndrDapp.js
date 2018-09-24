36920974b6352dcc88cf4aa12b6620903cf12acd
sknampally <satish@ikkurty.capital>
2018-09-24 12:41:49 +0530

    commit

--- byndrDapp.js
+++ byndrDapp.js
@@ -1,7 +1,11 @@
-const express = require('express')
+const express = require('express');
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
 const app = express()
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     res.render('index');
 })
@@ -9,5 +13,10 @@ app.get('/dashboard', function (req, res) {
     res.render('dashboard');
 })
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Dapp started on port 3000!')
 })
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -19,106 +19,108 @@ function GetParameterValues(param) {
     }  
 }
 
+var contractAddress = '0xeEE02c175e13EBe4a68147C5F44bD486f5AC7a6d';
+var mainContract = web3.eth.contract(abiMyLibrary).at(contractAddress);
+
+var totalBooks;
+var booksIssued;
+var totalMembers;
+
+var networkID;
+
+if (typeof web3 !== 'undefined') {
+    var web3 = new Web3(web3.currentProvider);
+    if (web3.currentProvider.isMetaMask === true || web3.currentProvider.isTrust === true) {
+        web3.version.getNetwork((err, netId) => {
+            networkID = netId;
+            if (netId == 3) {
+                // Do nothing
+            } else {
+                alert("You're not on right network. Switch to Kovan Network in Metamask.");
+            }
+            account = web3.eth.accounts[0];
+            ethBalance(account);
+        });
+    } else {
+        //$('.yourAddr').text('Not found');
+    }
+}
+
+function ethBalance(account) {
+    web3.eth.getBalance(account, function (err, res) {
+        if (!err) {
+            // Add the balance to UI
+        } else {
+            web3Error();
+            console.error(err);
+        };
+    });
+}
+
 $(document).ready(function() {
+    var state = GetParameterValues('state');
 
-var state = GetParameterValues('state');
+    if((state == "list_of_books") || (state == undefined)) {
+        $('#list_books_block').css('display','block');
+        $('#list_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td ><span class="issue_book" data-id="1">Issue Book</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book_disable">Book Issued</span></td></tr>');
+        $('#list_books').DataTable(); // data table constructor
 
-if((state == "list_of_books") || (state == undefined)) {
+    }
 
-    $('#list_books_block').css('display','block');
+    if(state == "available_books") {
+        $('#available_books_block').css('display','block');
+        $('#available_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td ><span class="issue_book" data-id="1">Issue Book</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book_disable">Book Issued</span></td></tr>');
+        $('#available_books').DataTable(); // data table constructor
+    }
 
-    $('#list_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td ><span class="issue_book" data-id="1">Issue Book</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book_disable">Book Issued</span></td></tr>');
+    if(state == "issue_book") {
+        $('#issue_book_block').css('display','block');
+        $('#issue_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td ><span class="issue_book" data-id="789465">Issue Book</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book" data-id="123456">Issue Book</span></td></tr>');
+        $('#issue_books').DataTable(); // data table constructor
+    }
 
-    $('#list_books').DataTable(); // data table constructor
+    if(state == "books_issued") {
+        $('#issued_books_block').css('display','block');
+        $('#issued_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td><span class="issue_book_disable">Book Issued</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book_disable">Book Issued</span></td></tr>');
+        $('#issued_books').DataTable(); // data table constructor
+    }
 
-}
-
-if(state == "available_books") {
-    
-    $('#available_books_block').css('display','block');
-
-    $('#available_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td ><span class="issue_book" data-id="1">Issue Book</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book_disable">Book Issued</span></td></tr>');
-
-
-    $('#available_books').DataTable(); // data table constructor
-
-}
-
-if(state == "issue_book") {
- 
-    $('#issue_book_block').css('display','block');
-
-    $('#issue_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td ><span class="issue_book" data-id="789465">Issue Book</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book" data-id="123456">Issue Book</span></td></tr>');
-
-
-    $('#issue_books').DataTable(); // data table constructor
-
-}
-
-if(state == "books_issued") {
-
-    $('#issued_books_block').css('display','block');
+    if(state == "total_members") {
+        $('#total_members_block').css('display','block');
+        $('#total_members tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td >Active</td></tr><tr><td>2</td><td>Tiger Nixon</td><td>System Architect</td><td>Active</td></tr>');
+        $('#total_members').DataTable(); // data table constructor
+    }
 
 
-    $('#issued_books tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td>Edinburgh</td><td>61</td><td><span class="issue_book_disable">Book Issued</span></td></tr><tr><td>2</td><td>Garrett Winters</td><td>Accountant</td><td>Tokyo</td><td>63</td><td ><span class="issue_book_disable">Book Issued</span></td></tr>');
+    /* Add book Functionality */
+
+    $('.add_book').on('click',function(){
+        $('.add_book_main').fadeIn();
+    });
 
 
-    $('#issued_books').DataTable(); // data table constructor
+    $('.add_book_close').on('click',function(){
+        $('.add_book_main').fadeOut();
+    });
 
-}
+    $('#add_book_form').submit(function(e){
+    e.preventDefault();
 
-if(state == "total_members") {
+    var book_name = $('#book_name').val();
+    var book_author = $('#book_author').val();
+    var book_genre = $('#book_genre').val();
 
-    $('#total_members_block').css('display','block');
-
-
-    $('#total_members tbody').append('<tr><td>1</td><td>Tiger Nixon</td><td>System Architect</td><td >Active</td></tr><tr><td>2</td><td>Tiger Nixon</td><td>System Architect</td><td>Active</td></tr>');
-
-
-    $('#total_members').DataTable(); // data table constructor
-
-}
-
-
-/* Add book Functionality */
-
-$('.add_book').on('click',function(){
-
-$('.add_book_main').fadeIn();
-
-});
-
-
-$('.add_book_close').on('click',function(){
-
-$('.add_book_main').fadeOut();
-
-});
-
-$('#add_book_form').submit(function(e){
-e.preventDefault();
-
-var book_name = $('#book_name').val();
-var book_author = $('#book_author').val();
-var book_genre = $('#book_genre').val();
-
-if(book_genre && book_author && book_genre) {
-
-// Add Book Functionality 
-
-// end add book functionality
-$('.add_book_status').removeClass('error');
-$('.add_book_status').addClass('success');
-$('.add_book_status').html('You Have added Book Successfully');
-
-}else{
-
-$('.add_book_status').removeClass('success');
-$('.add_book_status').addClass('error');
-$('.add_book_status').html('Please fill all the fields');
-
-}
-
+    if(book_genre && book_author && book_genre) {
+        // Add Book Functionality 
+        // end add book functionality
+        $('.add_book_status').removeClass('error');
+        $('.add_book_status').addClass('success');
+        $('.add_book_status').html('You Have added Book Successfully');
+    } else {
+        $('.add_book_status').removeClass('success');
+        $('.add_book_status').addClass('error');
+        $('.add_book_status').html('Please fill all the fields');
+    }
 });
 
 /* Add book Functionality */
